6b48aff1b3b37227ea5e18cdd2a7cf3ace4e2ddc
Axel <axelwass@gmail.com>
2012-04-02 03:58:36 -0300

    servlet sencillo que por ahora solo muestra la informacion de una publicacion.

---
+++ zonaProp/src/main/java/web/Publication.java
@@ -0,0 +1,33 @@
+package web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import services.PublicationService;
+
+public class Publication extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			int userId = Integer.parseInt(req.getParameter("userId"));
+			int publicationId = Integer.parseInt(req.getParameter("publicationId"));
+			PublicationService ps = PublicationService.getInstance();
+			req.setAttribute("publication", ps.getPublication(publicationId, userId));
+			req.getRequestDispatcher("/WEB-INF/jsp/publication.jsp").forward(req,
+					resp);
+		} catch (NumberFormatException nfe) {
+			resp.sendError(400);
+		}
+	}
+}
