a1099432cf1d15c5afe48b30c84849ff60c37773
agustinmarseillan <agustinmarseillan@gmail.com>
2012-04-12 15:21:12 -0300

    handling de errores

---
+++ zonaProp/src/main/java/filters/ErrorFilter.java
@@ -0,0 +1,34 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class ErrorFilter implements Filter{
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		HttpServletResponse resp = (HttpServletResponse) arg1;
+		try{
+			arg2.doFilter(request, resp);
+		}catch(Exception e){
+			resp.sendRedirect("errorPage");
+		}
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
---
+++ zonaProp/src/main/webapp/WEB-INF/jsp/errorPage.jsp
@@ -0,0 +1,7 @@
+
+<%@ include file="header.jsp" %>
+
+<h3>Error interno del sistema.</h3>
+Nuestro equipo de IT est&aacute; trabajando en la soluci&oacute;n.
+						
+<%@ include file="footer.jsp" %>
