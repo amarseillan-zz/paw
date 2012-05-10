21099deb7663ee36109d37d3e27350e5516ec9c9
Axel <axelwass@gmail.com>
2012-05-10 14:41:57 -0300

    se fija si los campos son positivos

--- zonaProp/src/main/java/zonaProp/web/command/validator/PublicationFormValidator.java
+++ zonaProp/src/main/java/zonaProp/web/command/validator/PublicationFormValidator.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import zonaProp.validators.DoubleValidator;
 import zonaProp.validators.FieldValidator;
-import zonaProp.validators.IntegerValidator;
 import zonaProp.validators.LengthValidator;
 import zonaProp.validators.NotNullValidator;
+import zonaProp.validators.PositiveDoubleValidator;
+import zonaProp.validators.PositiveIntValidator;
 import zonaProp.web.command.PublicationForm;
 
 
@@ -19,11 +19,11 @@ public class PublicationFormValidator implements Validator {
 	private FieldValidator<String> directionValdator = new LengthValidator("address", 1, 30);
 	private FieldValidator<String> cityValdator = new LengthValidator("city", 1, 30);
 	
-	private FieldValidator<String> precioValdator = new DoubleValidator("price");
-	private FieldValidator<String> coveredValdator = new DoubleValidator("covered");
-	private FieldValidator<String> uncoveredValdator = new DoubleValidator("uncovered");
+	private FieldValidator<String> precioValdator = new PositiveDoubleValidator("price");
+	private FieldValidator<String> coveredValdator = new PositiveDoubleValidator("covered");
+	private FieldValidator<String> uncoveredValdator = new PositiveDoubleValidator("uncovered");
 	
-	private FieldValidator<String> ageValdator = new IntegerValidator("age");
+	private FieldValidator<String> ageValdator = new PositiveIntValidator("age");
 	
 	private FieldValidator<Object> operationTypeValidator = new NotNullValidator("operationType");
 	private FieldValidator<Object> propertyTypeValidator = new NotNullValidator("propertyType");
